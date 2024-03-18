691ac115fd643e09a9c43c7399f833bd24f179c1
Steve Rushton <Steven.Rushton@CSUGlobal.edu>
2024-03-18 11:47:14 -0600

    Shell for resturant info print function

---
+++ src/PrintAddress.java
@@ -0,0 +1,9 @@
+
+public class PrintAddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("resturant info will go here"); //FIXME
+	}
+
+}
